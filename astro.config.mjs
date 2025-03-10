a756315d99585d5872995a821e93be4aa4f474a7
Twis06 <lipy.5101@gmail.com>
2025-03-10 10:53:21 +0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://winnież6.github.io',
+  output: 'static',
+  adapter: node({
+    mode: 'standalone'
+  })
+}); 
