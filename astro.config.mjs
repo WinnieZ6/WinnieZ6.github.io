83b463ce47ce4585b8e9b93e72ed501b758bc689
WinnieZ6 <lipeiyang@shphschool.com>
2025-03-10 11:03:38 +0800

    Fix build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://winnież6.github.io',
-  output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [tailwind()],
+  output: 'static'
 }); 
